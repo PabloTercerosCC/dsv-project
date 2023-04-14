b649d112cd94de683a59db4c94d37dcfd8b1b102
Melany_Argandona2002 <melany.argandona.60089@usalesiana.edu.bo>
2023-04-14 19:59:37 -0400

    add task or list for todolist

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import HeadToDoList from './HeadToDoList.svelte';
+    import Lists from './Lists.svelte';
+</script>
+
+<div class="toDoList">
+    <HeadToDoList />
+    <Lists />
+</div>
+
+<style>
+</style>
