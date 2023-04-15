ccf9285a586476b946ce1d5dd5fcfcad46795328
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-04-15 13:09:45 -0400

    edit implementation

--- src/routes/List.svelte
+++ src/routes/List.svelte
@@ -1,20 +1,45 @@
 <script>
-    export let name = '';
-    import Task from './Task.svelte';
+  export let name = '';
+  import Task from './Task.svelte';
 
-    function addNewTask(event) {
-      if (name) {
-        const newTask = new Task({
-          target: event.target.parentNode.querySelector('.ul-listTasks'),
-        });
-      }
+  function addNewTask(event) {
+    if (name) {
+      const newTask = new Task({
+        target: event.target.parentNode.querySelector('.ul-listTasks'),
+      });
     }
+    editMode = 0;
+  }
+
+  let editMode = 0; // Index of the currently editing label
+  let editedLabel = ''; // Value of the currently editing label
+
+  function startEditing() {
+    if (editedLabel !== name) {
+      editedLabel = name; // Set the initial value of the editing label
+      editMode = 1;
+      console.log(editMode);
+    }
+  }
+
+  function saveEditing() {
+    name = editedLabel; // Update the label in the labels array
+    editMode = 0; // Reset the editing index when editing is finished
+    console.log(editMode);
+  }
 </script>
 
 <div class="task">
-    <li class="title-Task">{name}</li>
-    <button class="button-addTask" type="button" on:click={addNewTask}>Add task</button>
-    <ul class="ul-listTasks"></ul>
+  <li class="title-Task" on:click={() => startEditing()}>
+    {#if editMode === 1}
+      <input type="text" bind:value={editedLabel} /> <!-- Use an input element for editing -->
+      <button on:click={saveEditing}>Save</button> <!-- Use a button to save changes -->
+    {:else}
+      {name}
+    {/if}
+  </li>
+  <button class="button-addTask" type="button" on:click={addNewTask}>Add task</button>
+  <ul class="ul-listTasks"></ul>
 </div>
 
 <style>
--- src/routes/NewTask.svelte
+++ src/routes/NewTask.svelte
@@ -1,10 +1,34 @@
 <script>
     export let inputValue = '';
+
+    let editMode = 0; // Index of the currently editing label
+    let editedLabel = ''; // Value of the currently editing label
+
+    function startEditing() {
+        if (editedLabel !== inputValue) {
+            editedLabel = inputValue; // Set the initial value of the editing label
+            editMode = 1;
+            console.log(editMode);
+        }
+    }
+
+    function saveEditing() {
+        inputValue = editedLabel; // Update the label in the labels array
+        editMode = 0; // Reset the editing index when editing is finished
+        console.log(editMode);
+    }
 </script>
 
 <div>
     <input type="checkbox" name="task">
-    <label for="task">{inputValue}</label>
+    <label for="task" on:click={() => startEditing()}>
+      {#if editMode === 1}
+        <input type="text" bind:value={editedLabel} /> <!-- Use an input element for editing -->
+        <button on:click={saveEditing}>Save</button> <!-- Use a button to save changes -->
+      {:else}
+        {inputValue}
+      {/if}
+    </label>
 </div>
 
 <style>
