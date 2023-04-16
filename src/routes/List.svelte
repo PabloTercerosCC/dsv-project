7a13abe25541a653dd5c7bba18ab85d09729542b
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-04-16 12:22:15 -0400

    edit implementation

--- src/routes/List.svelte
+++ src/routes/List.svelte
@@ -8,11 +8,36 @@
         target: event.target.parentNode.querySelector('.ul-listTasks'),
       });
     }
+    editMode = 0;
+  }
+
+  let editMode = 0;
+  let editedLabel = '';
+
+  function startEditing() {
+    if (editedLabel !== name) {
+      editedLabel = name; // Set the initial value of the editing label
+      editMode = 1;
+      console.log(editMode);
+    }
+  }
+
+  function saveEditing() {
+    name = editedLabel; // Update the label in the labels array
+    editMode = 0; // Reset the editing index when editing is finished
+    console.log(editMode);
   }
 </script>
 
 <div class="task">
-  <li class="title-Task">{name}</li>
+  <li class="title-Task" on:click={() => startEditing()}>
+    {#if editMode === 1}
+      <input type="text" bind:value={editedLabel} /> <!-- Use an input element for editing -->
+      <button on:click={saveEditing}>Save</button> <!-- Use a button to save changes -->
+    {:else}
+      {name}
+    {/if}
+  </li>
   <button class="button-AddTask" type="button" on:click={addNewTask}>Add task</button>
   <ul class="ul-listTasks"></ul>
 </div>
